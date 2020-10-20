4532f2592abd919d9e362af7013b89a96390f528
Carl Louie Pernito <carl@ratescalc.com>
2020-10-20 09:15:17 +0800

    Updates

---
+++ precache-manifest.80f4c4f3c93baa090691e97bd7e164dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dca89e2dab07cd8ee3d4f23401516a3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2cac20f7eb7acce9528b",
+    "url": "/monsters-rolodex/static/css/main.43dd6c04.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cac20f7eb7acce9528b",
+    "url": "/monsters-rolodex/static/js/main.8a9a0af4.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
